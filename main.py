4b4fc4e319eca5e6ccf5a8b45993c5ac239b42c4
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2023-11-03 15:03:08 +0100

    add

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+import json
+import requests
+from requests import get
 
 print("starting Tracefy© weather checker")
+
+# get configurable preference data from config.json
+with open('config.json', 'r') as config_file:
+    config_data = json.load(config_file)
+
+max_bike_distance = config_data["max_bike_distance"]
+max_bike_temp = config_data["max_bike_temp"]
+min_bike_temp = config_data["min_bike_temp"]
+max_wind_speed = config_data["max_wind_speed"]
+
+ip = get('https://api.ipify.org').content.decode('utf8')
+
+location_key = ""
+api_key = "4A8s1R9XEApO2lZV3FseGqfV198SawHy"
+
+url = f"http://dataservice.accuweather.com/locations/v1/cities/ipaddress?q={ip}&apikey={api_key}"
+
+response = requests.get(url)
+
+conditionsUrl = f""
