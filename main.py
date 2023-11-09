bcaed20cae568545eca4f4246c22930e5303b170
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2023-11-09 14:20:19 +0100

    part 2

--- main.py
+++ main.py
@@ -34,35 +34,26 @@ def get_weather_from_location_api(location_key, api_key, api_url_base):
     conditionsResponse = requests.get(conditions_url, params=conditions_params)
     return conditionsResponse.json()
 
+
 def get_time_from_db():
-    time_data = sql_client.fetch_all(
-        "SELECT * WHERE `id` = (SELECT MAX(`id`) FROM Weather_table);"
-    )
+    if sql_client.check_table_exists("weather_table"):
+        time = sql_client.fetch_all(
+            "SELECT saved_at FROM weather_table WHERE `id` = (SELECT MAX(`id`) FROM weather_table);"
+        )
+        time = time[0]  # Assuming there's only one dictionary in the list
+        return time['saved_at']
+    else:
+        return "1970-01-01 00:00:00"
+
 
 def get_weather_from_db():
-    conditions_data = sql_client.fetch_all(
-        "SELECT * WHERE `id` = (SELECT MAX(`id`) FROM Weather_table);"
-    )
+    if check_tabel_existence:
+        return sql_client.fetch_all(
+            "SELECT * FROM weather_table WHERE `id` = (SELECT MAX(`id`) FROM weather_table);"
+        )
 
 
-def get_weather_from_ip():
-    with open('config.json', 'r') as config_file:
-        config_data = json.load(config_file)
-
-    current_time = datetime.datetime.now()
-    one_hour_ago = current_time - datetime.timedelta(hours=1)
-
-    formatted_current_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    formatted_one_hour_ago = one_hour_ago.strftime("%Y-%m-%d %H:%M:%S")
-
-    max_bike_distance = config_data["max_bike_distance"]
-    max_bike_temp = config_data["max_bike_temp"]
-    min_bike_temp = config_data["min_bike_temp"]
-
-
-
-    # conditions_data = get_weather_from_db()
-
+def api_response(formatted_current_time, max_bike_distance, max_bike_temp, min_bike_temp, config_data):
     ip = get_ip
 
     location_data = get_location_from_ip_api(ip, api_key, api_url_base)
@@ -74,7 +65,7 @@ def get_weather_from_ip():
     create_tables()
 
     current_weather = conditions_data[0]['WeatherText']
-    current_weather_ic = conditions_data[0]['WeatherIcon']
+    current_weather_icon = conditions_data[0]['WeatherIcon']
     current_temp = conditions_data[0]['Temperature']['Metric']['Value']
     lat = location_data[0]['GeoPosition']['Latitude']
     lon = location_data[0]['GeoPosition']['Longitude']
@@ -89,59 +80,119 @@ def get_weather_from_ip():
     bike_distance = check_bike_distance(distance, max_bike_distance)
 
     response_data = {
-        "Temperature": current_temp,
-        "Weather": current_weather,
-        "icon": current_weather_ic,
+        "temperature": current_temp,
+        "weather": current_weather,
+        "icon": current_weather_icon,
         "bikeable": check_bike(temp, bike_distance),
-        "Lat": lat,
-        "Lon": lon,
-        "Saved_at": formatted_current_time,
+        "latitude": lat,
+        "longitude": lon,
+        "saved_at": formatted_current_time,
     }
     insertable = {
-        "Temperature": current_temp,
-        "Weather": current_weather,
-        "icon": current_weather_ic,
-        "Latitude": lat,
-        "Longitude": lon,
-        "Saved_at": formatted_current_time,
+        "temperature": current_temp,
+        "weather": current_weather,
+        "icon": current_weather_icon,
+        "latitude": lat,
+        "longitude": lon,
+        "saved_at": formatted_current_time,
     }
     insertdata(insertable)
 
     return response_data
 
 
-def create_tables():
-    weather_table = "Weather_table"
+def db_response(max_bike_distance, max_bike_temp, min_bike_temp, config_data):
+    conditions_data = get_weather_from_db()
 
-    # Check if Weather table exists
+    current_weather = conditions_data[0]['weather']
+    current_weather_icon = conditions_data[0]['icon']
+    current_temp = conditions_data[0]['temperature']
+    lat = conditions_data[0]['latitude']
+    lon = conditions_data[0]['longitude']
+    saved_at = conditions_data[0]['saved_at']
+    current_float_temp = float(current_temp)
+
+    max_bike_temp = float(max_bike_temp)
+    min_bike_temp = float(min_bike_temp)
+
+    distance = config_data["bike_distance"]
+
+    temp = check_temprature(current_float_temp, max_bike_temp, min_bike_temp)
+    bike_distance = check_bike_distance(distance, max_bike_distance)
+
+    response_data = {
+        "temperature": current_temp,
+        "weather": current_weather,
+        "icon": current_weather_icon,
+        "bikeable": check_bike(temp, bike_distance),
+        "latitude": lat,
+        "longitude": lon,
+        "saved_at": saved_at,
+    }
+    return response_data
+
+
+def get_weather_from_ip_main():
+    with open('config.json', 'r') as config_file:
+        config_data = json.load(config_file)
+
+    current_time = datetime.datetime.now()
+    one_hour_ago = current_time - datetime.timedelta(hours=1)
+
+    formatted_current_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    formatted_one_hour_ago = one_hour_ago.strftime("%Y-%m-%d %H:%M:%S")
+
+    max_bike_distance = config_data["max_bike_distance"]
+    max_bike_temp = config_data["max_bike_temp"]
+    min_bike_temp = config_data["min_bike_temp"]
+
+    time_data_db = get_time_from_db()
+
+    if not time_data_db >= formatted_one_hour_ago:
+        response_data = api_response(formatted_current_time, max_bike_distance, max_bike_temp, min_bike_temp,
+                                     config_data)
+    else:
+        response_data = db_response(max_bike_distance, max_bike_temp, min_bike_temp, config_data)
+
+    return response_data
+
+
+def check_tabel_existence():
+    sql_client.check_table_exists("weather_table")
+
+
+def create_tables():
+    weather_table = "weather_table"
+
+    # Check if weather table exists
     if not sql_client.check_table_exists(weather_table):
-        # Create Weather table if it doesn't exist
+        # Create weather table if it doesn't exist
         create_weather_table_query = """
-            CREATE TABLE Weather_table (
+            CREATE TABLE weather_table (
                 id INT AUTO_INCREMENT PRIMARY KEY,
-                Weather VARCHAR(255) NOT NULL,
-                Icon INT NOT NULL,
-                Temperature FLOAT NOT NULL,
-                Latitude FLOAT NOT NULL,
-                Longitude FLOAT NOT NULL,
-                Saved_at VARCHAR(255) NOT NULL
+                weather VARCHAR(255) NOT NULL,
+                icon INT NOT NULL,
+                temperature FLOAT NOT NULL,
+                latitude FLOAT NOT NULL,
+                longitude FLOAT NOT NULL,
+                saved_at VARCHAR(255) NOT NULL
             );"""
         sql_client.query_fix(create_weather_table_query)
 
 
 def insertdata(insertable):
-    Weather = insertable.get("Weather")
+    weather = insertable.get("weather")
     Icon = insertable.get("icon")
-    Temperature = insertable.get("Temperature")
-    Latitude = insertable.get("Latitude")
-    Longitude = insertable.get("Longitude")
-    Saved_at = insertable.get("Saved_at")
+    temperature = insertable.get("temperature")
+    latitude = insertable.get("latitude")
+    longitude = insertable.get("longitude")
+    saved_at = insertable.get("saved_at")
 
-    keys = ["Weather", "icon", "Temperature", "Latitude", "Longitude", "Saved_at"]
+    keys = ["weather", "icon", "temperature", "latitude", "longitude", "saved_at"]
     keys = tuple(keys)
-    values = [Weather, Icon, Temperature, Latitude, Longitude, Saved_at]
+    values = [weather, Icon, temperature, latitude, longitude, saved_at]
     values = tuple(values)
-    table_name = "Weather_table"
+    table_name = "weather_table"
     sql_client.insert(keys, values, table_name)
 
 
@@ -156,8 +207,7 @@ def check_temprature(current_temp, maxtemp, mintemp):
     else:
         maxtemp_chk = False
 
-    if maxtemp_chk == True and mintemp_chk == True:
-        return True
+    return maxtemp_chk and mintemp_chk
 
 
 def check_bike_distance(distance, max_bike_distance):
@@ -170,8 +220,10 @@ def check_bike_distance(distance, max_bike_distance):
 def check_bike(temp, distance):
     if temp and distance:
         return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
-    weather = get_weather_from_ip()
+    weather = get_weather_from_ip_main()
     print(weather)
